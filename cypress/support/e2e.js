2ff1a6e31044085540273c6c4aade62ec5852c30
yarreaza <arreazay@gmail.com>
2024-02-04 22:02:11 +0100

    Subida la repositorio remoto

---
+++ cypress/support/e2e.js
@@ -0,0 +1,18 @@
+// ***********************************************************
+// Este ejemplo support/e2e.js se procesa y
+// cargado automáticamente antes de sus archivos de prueba.
+//
+// Este es un gran lugar para poner la configuración global y
+// comportamiento que modifica Cypress.
+//
+// Puedes cambiar la ubicación de este archivo o desactivarlo.
+// servir automáticamente archivos de soporte con el
+// opción de configuración 'supportFile'.
+//
+// Puede leer más aquí:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
